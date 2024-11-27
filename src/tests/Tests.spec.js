654d15a8f098a0210fc1895b99764c280c09a533
Jhayne Ketleen Henemam Martins <jhayne.56221@alunosatc.edu.br>
2024-11-27 20:31:32 -0300

    Teste Grafana K6 perfomance

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,24 +1,27 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+// Métricas Personalizadas
+export const tempoChamadaGET = new Trend('tempo_chamada_GET', true);
+export const taxaErro = new Rate('taxa_erro');
 
+// Configurações do Teste
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '1m', target: 10 },    // Ramp-up para 10 VUs em 1 minuto
+    { duration: '3m', target: 300 },   // Ramp-up para 300 VUs em 3 minutos
+    { duration: '1m', target: 0 }      // Ramp-down para 0 VUs em 1 minuto
+  ],
+  thresholds: {
+    'tempo_chamada_GET': ['p(95)<5700'], // 95% das respostas abaixo de 5700ms
+    'taxa_erro': ['rate<0.12']           // Menos de 12% de erros nas requisições
+  }
 };
 
+// Função para Gerar Relatórios
 export function handleSummary(data) {
   return {
     './src/output/index.html': htmlReport(data),
@@ -26,8 +29,9 @@ export function handleSummary(data) {
   };
 }
 
+// Função Principal do Teste
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://catfact.ninja/fact'; // URL da API Cat Facts
 
   const params = {
     headers: {
@@ -35,15 +39,22 @@ export default function () {
     }
   };
 
-  const OK = 200;
-
+  // Realiza a requisição GET
   const res = http.get(`${baseUrl}`, params);
 
-  getContactsDuration.add(res.timings.duration);
+  // Registra a duração da chamada GET
+  tempoChamadaGET.add(res.timings.duration);
 
-  RateContentOK.add(res.status === OK);
+  // Verifica se o status code indica sucesso (200-399)
+  const sucesso = res.status >= 200 && res.status < 400;
 
+  // Registra a taxa de erro
+  taxaErro.add(!sucesso);
+
+  // Verifica se o status code é 200
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET - Status 200': () => res.status === 200
   });
+
+  sleep(1); // Simula o tempo entre as requisições
 }
